0d2b69b8a16ae10d5f205923106204f5daa47c99
yakushiiii <sofiayakushi@gmail.com>
2026-02-15 19:30:32 -0800

    all keys

--- keys.py
+++ keys.py
@@ -1,8 +1,9 @@
 serverA_private_key = "3876593914d5f5a60daa5380ccfe961b63a0890b2826219f5b05a7a5a1496d45"
 serverA_public_key = "be4420b424085b51bad775e8384cc15c6ebc22c49cf169cd8635f56caa28283e"
 
-serverB_private_key = 0
-serverB_public_key = 0
+serverB_private_key = "3802f12fe254e95f1b793c03ed4d41ce3f4204b9f664867204c4a75f0544986c"
+serverB_public_key = "cee95351c9c12d143aa18aaf0e665b3f24aa7056d092c7f75131736ac0de6944"
 
-serverC_private_key = 0
-serverC_public_key = 0
+serverC_private_key = "6086e59807c08d25860310fcc6921a19b0aeaad25a21895160dc689a69d3ef6d"
+serverC_public_key = "c3347a3b97843e798bf56dee5fa4485bc7453476ca47789f9b954b5496daaa1e"
+
