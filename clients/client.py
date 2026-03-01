ca058640083b87ce05aad16b2f425b9f7fd6cd2b
yakushiiii <sofiayakushi@gmail.com>
2026-03-01 15:36:21 -0800

    progress towards up and running

--- clients/client.py
+++ clients/client.py
@@ -4,20 +4,20 @@ import socket
 import asyncio 
 import os
 import hashlib
+import struct
+import json
 from cryptography.hazmat.primitives.asymmetric import x25519
 from cryptography.hazmat.primitives.asymmetric import dh
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-from config import GLOBAL_KEY_LEN, GLOBAL_SALT
+import config
 from keys import serverA_public_key, serverB_public_key, serverC_public_key
 
-server_1 = "127.0.0.1"
+server_A = "127.0.0.1"
 port = 9000
-
-client_id_1 = 1 #for now hardcoding client id
-client_id_2 = 2 #for now harcoding other client id
+server_A_pubK = "e4420b424085b51bad775e8384cc15c6ebc22c49cf169cd8635f56caa28283"
 
 round_number = 1 #for now hardcoding the round_number
 
@@ -27,8 +27,29 @@ class Client:
         #long term keypair for use
         self.private_key = x25519.X25519PrivateKey.generate()
         self.public_key = self.private_key.public_key()
+        config.GLOBAL_CLIENT_COUNT += 1
+        self.username = "client" + str(config.GLOBAL_CLIENT_COUNT) #for now hardcoding the username by the client number 
+        #sending public key to directory.json file PUT THIS IN SERVER CODE
+        with open("directory.json", "r") as file:
+            data = json.load(file)
 
+        data["users"][self.username] = {"public_key": self.public_key}
+
+        with open("directory.json", "w") as file:
+            json.dump(data, file, indent=4)
+
+    #registers user by sending the information the server needs to add them to the the directory json
+    def register_user():
+        return
     
+    #ask entry server (serverA) for the json file to retrieve the user they want to communicate with:
+    def retrieve_user_pubK(client_name):
+        return
+
+    #-------------------------------------------
+    # Encryption
+    #-------------------------------------------
+
     #creating a shared secret
     def shared_secret(self, client_2_public_key):
         shared_key = self.private_key.exchange(client_2_public_key)
@@ -41,8 +62,8 @@ class Client:
 
         encryption_key = HKDF(
             algorithm = hashes.SHA256(),
-            length = GLOBAL_KEY_LEN,
-            salt = GLOBAL_SALT,
+            length = config.GLOBAL_KEY_LEN,
+            salt = config.GLOBAL_SALT,
             info = info,
         ).derive(shared_key)
         return encryption_key
@@ -80,8 +101,8 @@ class Client:
         sh_key = server_epk.exchange(server_public_key)
         key = HKDF(
             algorithm = hashes.SHA256(),
-            length = GLOBAL_KEY_LEN,
-            salt = GLOBAL_SALT,
+            length = config.GLOBAL_KEY_LEN,
+            salt = config.GLOBAL_SALT,
             info = info,
         ).derive(sh_key)
 
@@ -110,26 +131,61 @@ if __name__ == "__main__":
 
     shared1 = client_1.shared_secret(client_2.public_key)
     shared2 = client_2.shared_secret(client_1.public_key)
+    assert shared1 == shared2
 
     df_client_1 = client_1.diffie_hellman(client_2.public_key)
     df_client_2 = client_2.diffie_hellman(client_1.public_key)
     assert df_client_1 == df_client_2
 
-    while(1):
-        message = input('Enter your message:')
+    print("Welcome to our anonymouse private metadata messaging service!")
+    print("Please ensure you leave this program running in the background in order to maintain privacy.")
 
-        if message == "":
-            message = "NO MESSAGE" 
-            
-        #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #sock.connect(port)
+    while (1): #this while loop is for initiating the conversationg between two clients
+        connect_client = input("Enter the username of client you want to communicate with: ")
+        #change this to ask server1 the directory.json file
+        if (connect_client == "" | connect_client == '\n' ):
+            print("Not a valid user.")
+            connect_client = input("Enter the username of client you want to communicate with: ")
 
-        ciphertext = client_1.encrypt_message(df_client_1, message, round_number)
-        dead_drop_id = client_1.get_dead_drop_id(shared1, round_number)
+        #find a way to ask the server for the public key of the client using the username. For now just hardcoding it
+        shared1 = client_1.shared_secret(client_2.public_key)
+        df_client_1 = client_1.diffie_hellman(client_2.public_key)
 
-        #onion encrypt the message
-        onion_msg = client_1.onion_encrypt(ciphertext, dead_drop_id)
+        print("You are now communicating with", connect_client)
+        print('Enter "\\quit" to end the conversation')
+        while(1): #this while loop is for sending messages between two clients, we can break out of this loop to end the conversation
 
+            # creating the message to send
+            message = input('> ')
+
+            if message == "\\quit":
+                break
+
+            #if no message then fill message with "NO MESSAGE" to prevent leaking information about message length
+            if message == "":
+                message = "NO MESSAGE" 
+                
+            #create socket and connect to server
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((server_A, port))
+
+            #encrypting message and establishing dead drop id
+            ciphertext = client_1.encrypt_message(df_client_1, message, round_number)
+            dead_drop_id = client_1.get_dead_drop_id(shared1, round_number)
+
+            #onion encrypt the message
+            onion_msg = client_1.onion_encrypt(ciphertext, dead_drop_id)
+
+            #need to send the message in bytes so using struct to do this
+            length = len(onion_msg)
+            length_bytes = struct.pack("!I", length) 
+
+            #sending the onion encrypted message to the server
+            sock.sendall(length_bytes + onion_msg)
+
+            #receiving data
+            response = sock.recv(4096)
+    
         
-        #sock.send(onion_message)
-        #esponse = sock.recv(4096)
+            sock.close()
+
