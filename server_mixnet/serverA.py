7c9bee2851bec87f2f019f8215dd2e85fd2322bb
rachel <143126246+duckdeathray@users.noreply.github.com>
2026-03-01 18:43:54 -0800

    Edit 8

--- server_mixnet/serverA.py
+++ server_mixnet/serverA.py
@@ -14,19 +14,31 @@ from shuffle import shuffle, unshuffle
 
 ## RECEIVE FROM CLIENT
 
+def setup():
+    # Initialize dead drop with None values for each bucket
+    global NUM_BUCKETS 
+    NUM_BUCKETS = 100
+    dead_drop = [None, None] * NUM_BUCKETS
+
+def get_bucket_index(dead_drop_id):
+    # Returns index of bucket for this message
+    return int(dead_drop_id, 32) % NUM_BUCKETS
+
 def get_new_message():
     # Returns index of message for this round, encrypted content
     # Listens on socket for new messages from client
-    pass
+    
+# dead_drop_index(hash)(bytes) || encrypted message
 
 # Receives messages from the client and stores them in a list
 def receive_messages_from_client(ROUND_LEN):
+
     start_time = time.time()                            # Start timer for batch round
     messages_list = []                                  # Initialize empty list to store messages
 
     while time.time() - start_time < ROUND_LEN:          # Loop to receive messages until batch round ends
         new_message = get_new_message()                 # Function to receive a new message from the client
-        messages_list.append(new_message)
+        messages_list.append(dead_drop[new_message])
 
 # Decrypts first layer of encryption using server A's private key
 def serverA_decrypt(messages_list):
